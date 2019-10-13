bad9195853aa0ccbbd4ce01c9c69ce567921b876
indra sadik <g3n1k@yahoo.com>
2019-10-13 07:01:51 +0700

    kemampuan dasar

---
+++ kemampuan-dasar-1/latihan/shellscripts/abc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 01
+echo "Hello World"
+
+# 02
+echo "Printing text with newline"
+echo -n "Printing text without newline"
+echo -e "\nRemoving \t backslash \t characters\n"
+
+# 03
+# Add two numeric value
+((sum=25+35))
+
+#Print the result
+echo $sum
+# result 25
+
+# 04nano 
